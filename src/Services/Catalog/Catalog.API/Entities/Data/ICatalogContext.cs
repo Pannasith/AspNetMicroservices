46762c7c5030f8109a68cb9533ba0f23063d0a29
git <seta_senpai@hotmail.com>
2021-07-16 21:03:02 +0700

    basket microservices developed

---
+++ src/Services/Catalog/Catalog.API/Entities/Data/ICatalogContext.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
